d640d17f928920f5b65323386600ae66c065d3cb
Minajolie <fayeamina303@gmail.com>
2024-06-23 14:26:13 -0700

    ajout de nouvelle fichiers

---
+++ donnees.js
@@ -0,0 +1,8 @@
+// declarer une variable de type int
+let nombre = 12;
+// convertir la variable de type int en string
+let convert = nombre.toString();
+alert(convert);
+// Déclarer une variable de type date afficher la date en prenant en compte la date d’aujourd’hui (jj-mm-yyyy)
+let date = new Date();
+alert(date)
